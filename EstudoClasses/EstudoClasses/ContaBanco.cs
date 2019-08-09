f8ff9ee0c927d15e41f863d45f61faef45bca6ac
VitorMioto <VitorMioto@users.noreply.github.com>
2019-08-09 00:38:36 -0300

    Criando classes em CSharp

---
+++ EstudoClasses/EstudoClasses/ContaBanco.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EstudoClasses
+{
+    class ContaBanco
+    {
+        public int Conta { get; private set; }
+        public string NomeTitular { get; set; }
+        public double Saldo { get; private set; }
+
+        ContaBanco(int conta, string nome, double saldo = 0.0)
+        {
+            Conta = conta;
+            NomeTitular = nome;
+            Saldo = saldo;
+        }
+
+        public void Saque(double valor)
+        {
+            Saldo -= (valor + 5.00);
+        }
+        public void Deposito(double valor)
+        {
+            Saldo += valor;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Conta: {0} - Titular: {1} Saldo: R$ {2}", Conta, NomeTitular, Saldo);
+        }
+    }
+}
---
+++ EstudoClasses/EstudoClasses/Estoque.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EstudoClasses
+{
+    class Estoque
+    {
+        public String Nome;
+        public double Preco;
+        public int Qtd;
+
+        public double ValorTotalEstoque()
+        {
+            return Qtd * Preco;
+
+        }
+        public void AdicionarProduto(int qtd)
+        {
+            Qtd += qtd;
+        }
+        public void RemoverProduto(int qtd)
+        {
+            Qtd -= qtd;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0}, R$ {1} Qtd: {2}. Valor total R$ {3}", Nome, Preco, Qtd, ValorTotalEstoque());
+        }
+    }
+}
